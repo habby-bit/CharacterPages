6fbfa33fcc21df856e3c878ed4ac2b6c21d5c778
Habby Olusesi <habbyolu@gmail.com>
2021-04-15 14:03:26 -0400

    switched to boot, created initial cards

--- character_pages/src/components/Cards.js
+++ character_pages/src/components/Cards.js
@@ -1,21 +1,53 @@
 import React from "react"
-import "../Styles/Home.css"
-import Card from "@material-ui/core/Card"
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import "../Styles/Cards.css"
+import Card from "react-bootstrap/Card"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+
 
 const Cards = () => {
 
     return (
     <div >
-        <Card>
-        <div className="Card">
-            <CardContent>
-            </CardContent>
-            <Button>
-            </Button>
-        </div>
-        </Card>
+        <Row className="justify-content-md-center">
+            <Col lg ={4}>
+                <Card className= "Card text-center" border = "dark">
+                <Card.Header style={{fontSize: 30}}>Trying Times</Card.Header>
+                <Card.Body>
+                    <Card.Subtitle> TV Drama </Card.Subtitle>
+                    <Card.Text>
+                        <p>A ton but not too much text giving a synopsis for the show</p>
+                    </Card.Text>
+                    <Button>Explore The Characters</Button>
+                </Card.Body>
+                </Card>
+            </Col>
+            <Col lg ={4}>
+                <Card className= "Card text-center" border = "dark">
+                <Card.Header style={{fontSize: 30}}>Running Backwards</Card.Header>
+                <Card.Body>
+                    <Card.Subtitle> Feature Drama </Card.Subtitle>
+                    <Card.Text>
+                        <p>A ton but not too much text giving a synopsis for the show</p>
+                    </Card.Text>
+                    <Button>Explore The Characters</Button>
+                </Card.Body>
+                </Card>
+            </Col>
+            <Col lg ={4}>
+                <Card className= "Card text-center" border = "dark">
+                <Card.Header style={{fontSize: 30}}>Pull/Sacrifices</Card.Header>
+                <Card.Body>
+                    <Card.Subtitle> Play Drama </Card.Subtitle>
+                    <Card.Text>
+                        <p>A ton but not too much text giving a synopsis for the show</p>
+                    </Card.Text>
+                    <Button>Explore The Characters</Button>
+                </Card.Body>
+                </Card>
+            </Col>
+        </Row>
     </div>
     )
 }
--- character_pages/src/pages/Home.js
+++ character_pages/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "../Styles/Home.css"
 import Cards from "../components/Cards"
+
 const Home = () => {
 
     return (
     <div >
+        <h1> Shows: </h1>
         <Cards />
     </div>
     )
