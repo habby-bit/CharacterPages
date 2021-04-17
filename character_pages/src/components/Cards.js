e6f22245619db3073fb2d5af6a6f7cd6771eff87
Habby Olusesi <habbyolu@gmail.com>
2021-04-17 11:30:58 -0400

    fixing margins

--- character_pages/src/components/Cards.js
+++ character_pages/src/components/Cards.js
@@ -4,14 +4,14 @@ import Card from "react-bootstrap/Card"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Button from "react-bootstrap/Button"
-
+import Navbar from "react-bootstrap/Navbar"
 
 const Cards = () => {
 
     return (
     <div >
-        <Row className="justify-content-md-center">
-            <Col lg ={4}>
+        <Row className="justify-content-md-center ">
+            <Col lg ={4} className="cardss">
                 <Card className= "Card text-center" border = "dark">
                 <Card.Header style={{fontSize: 30}}>Trying Times</Card.Header>
                 <Card.Body>
@@ -23,7 +23,7 @@ const Cards = () => {
                 </Card.Body>
                 </Card>
             </Col>
-            <Col lg ={4}>
+            <Col lg ={4} className="cardss">
                 <Card className= "Card text-center" border = "dark">
                 <Card.Header style={{fontSize: 30}}>Running Backwards</Card.Header>
                 <Card.Body>
@@ -35,7 +35,7 @@ const Cards = () => {
                 </Card.Body>
                 </Card>
             </Col>
-            <Col lg ={4}>
+            <Col lg ={4} className="cardss">
                 <Card className= "Card text-center" border = "dark">
                 <Card.Header style={{fontSize: 30}}>Pull/Sacrifices</Card.Header>
                 <Card.Body>
