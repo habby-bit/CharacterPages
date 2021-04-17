550f19d3d1abfbc06f8f378bedc1b69409f9b288
Habby Olusesi <habbyolu@gmail.com>
2021-04-17 12:39:30 -0400

    color scheme trial

--- character_pages/src/components/Cards.js
+++ character_pages/src/components/Cards.js
@@ -12,38 +12,38 @@ const Cards = () => {
     <div >
         <Row className="justify-content-md-center ">
             <Col lg ={4} className="card-col">
-                <Card className= "Card text-center" border = "dark">
+                <Card className= "Card text-center">
                 <Card.Header >Trying Times</Card.Header>
                 <Card.Body>
                     <Card.Subtitle> TV Drama </Card.Subtitle>
                     <Card.Text>
                         <p>A ton but not too much text giving a synopsis for the show</p>
                     </Card.Text>
-                    <Button>Explore The Characters</Button>
+                    <Button variant="dark">Explore The Characters</Button>
                 </Card.Body>
                 </Card>
             </Col>
             <Col lg ={4} className="card-col">
-                <Card className= "Card text-center" border = "dark">
+                <Card className= "Card text-center">
                 <Card.Header >Running Backwards</Card.Header>
                 <Card.Body>
                     <Card.Subtitle> Feature Drama </Card.Subtitle>
                     <Card.Text>
                         <p>A ton but not too much text giving a synopsis for the show</p>
                     </Card.Text>
-                    <Button>Explore The Characters</Button>
+                    <Button variant="dark" >Explore The Characters</Button>
                 </Card.Body>
                 </Card>
             </Col>
             <Col lg ={4} className="card-col">
-                <Card className= "Card text-center" border = "dark">
+                <Card className= "Card text-center">
                 <Card.Header >Pull/Sacrifices</Card.Header>
                 <Card.Body>
                     <Card.Subtitle> Play Drama </Card.Subtitle>
                     <Card.Text>
                         <p>A ton but not too much text giving a synopsis for the show</p>
                     </Card.Text>
-                    <Button>Explore The Characters</Button>
+                    <Button variant="dark" >Explore The Characters</Button>
                 </Card.Body>
                 </Card>
             </Col>
