f8b6ca58db4366c17e7f90dd2ba11710c1c45f6a
Habby Olusesi <habbyolu@gmail.com>
2021-04-20 13:42:58 -0400

    added character cards

--- character_pages/src/pages/TryingCharacters.js
+++ character_pages/src/pages/TryingCharacters.js
@@ -16,28 +16,88 @@ const TryingCharacters = () => {
 
     return (
     <div >
-        <h1 style={{fontStyle: "italic"}}>Trying Times</h1>
-        <p>Jaylin, Isaiah, Batman, Kia, Derek, Sebastian</p>
         <Button
             onClick={(e) => backHome(e)}
         >
             Home
         </Button>
-
+        <h1 style={{fontStyle: "italic"}}>Trying Times</h1>
+        <p>Jaylin, Isaiah, Batman, Kia, Derek, Sebastian</p>
         <Row className="justify-content-md-center">
             <Col lg={10}>
-                <Card>
-                    <Card.Header>Featured</Card.Header>
+                <Card style={{marginBottom: "30px"}}>
+                    <Card.Header>Jaylin</Card.Header>
                     <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
+                        <Card.Title>Artist, Apathetic</Card.Title>
                         <Card.Text>
-                            With supporting text below as a natural lead-in to additional content.
+                            Character description
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Her Page</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{marginBottom: "30px"}}>
+                    <Card.Header>Jesse</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Teacher, Loveable</Card.Title>
+                        <Card.Text>
+                            Character description
+                        </Card.Text>
+                        <Button variant="primary">His Page</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{marginBottom: "30px"}}>
+                    <Card.Header>Olivia</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Nurse, Kind</Card.Title>
+                        <Card.Text>
+                            Character description
+                        </Card.Text>
+                        <Button variant="primary">Her Page</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{marginBottom: "30px"}}>
+                    <Card.Header>Kia</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Lawyer, Type-A</Card.Title>
+                        <Card.Text>
+                            Character description
+                        </Card.Text>
+                        <Button variant="primary">Her Page</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{marginBottom: "30px"}}>
+                    <Card.Header>Sebastian</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Lawyer, Ass</Card.Title>
+                        <Card.Text>
+                            Character description
+                        </Card.Text>
+                        <Button variant="primary">His Page</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{marginBottom: "30px"}}>
+                    <Card.Header>Isaiah</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Chef, Good-Hearted</Card.Title>
+                        <Card.Text>
+                            Character description
+                        </Card.Text>
+                        <Button variant="primary">His Page</Button>
+                    </Card.Body>
+                </Card>
+                <Card >
+                    <Card.Header>Batman</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Student, Wise</Card.Title>
+                        <Card.Text>
+                            Character description
+                        </Card.Text>
+                        <Button variant="primary">His Page</Button>
                     </Card.Body>
                 </Card>
             </Col>
         </Row>
+
     </div>
     )
 }
