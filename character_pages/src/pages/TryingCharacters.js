31e1a6519089502273d69d424ea400f7fcb23388
Habby Olusesi <habbyolu@gmail.com>
2021-04-20 13:21:45 -0400

    button home

--- character_pages/src/pages/TryingCharacters.js
+++ character_pages/src/pages/TryingCharacters.js
@@ -1,11 +1,24 @@
 import React from "react"
 import "../Styles/TryingCharacters.css"
+import Button from "react-bootstrap/Button"
+import { useHistory } from "react-router-dom"
 
 const TryingCharacters = () => {
+
+    const history = useHistory();
+
+    const backHome = (e) => {
+        history.push("/")
+    }
 
     return (
     <div >
         <h1>TRYING CHARACTERS</h1>
+        <Button
+            onClick={(e) => backHome(e)}
+        >
+            Home
+        </Button>
     </div>
     )
 }
